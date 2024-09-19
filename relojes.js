2e02457eb8f24a50cefb77dd920b79a75d97012f
marcos tonfor <94049496+marcostonfor@users.noreply.github.com>
2024-09-19 14:45:20 +0200

    Update relojes.js

--- relojes.js
+++ relojes.js
@@ -82,8 +82,7 @@ if (salida24h) {
     font-size: 13pt;
     padding: 0.5vh 1vw;
     box-shadow: inset 0 0 10px 10px hsla(0, 0%, 41%, 0.397);
-    font-family: 'Comic Sans MS';
-    color: black;
+    font-family: monospace, consolas, sans serif;
     text-shadow: 0.1vh 0.1vw 1px orange;
     border-radius: 0.5vw;
 }
