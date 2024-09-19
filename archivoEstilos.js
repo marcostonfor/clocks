8950125d7e61aa1b0e07c0182beb25b87a7c4f40
marcos tonfor <94049496+marcostonfor@users.noreply.github.com>
2024-09-19 12:50:25 +0200

    Update archivoEstilos.js

--- archivoEstilos.js
+++ archivoEstilos.js
@@ -1,3 +1,23 @@
 export const estilosPersonales = `
+    #caja12h {
+        display: flex;
+    }
 
+    summary::marker {
+        content: none !important;
+    }
+
+    summary {
+        cursor: pointer !important;
+        color: orangered;
+        text-decoration: underline;
+        margin: 0.5vh auto;
+        font-size: 12pt;
+    }
+
+
+    #reloj12h {
+        margin-left: 2vw;
+        color: crimson;
+    }
     `;
